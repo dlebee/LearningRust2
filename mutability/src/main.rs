fcb2df194b1cc7f937c51fd87e5685406ba6e102
David Lebee <david.lebee@gluwa.com>
2022-12-23 02:25:53 -0500

    mutability

---
+++ mutability/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+    // by default rust variables 
+    // are immutable
+    let _imutable_number = 32;
+
+    // you can change a
+    // variables to mutable.
+    let mut _mutable_number = 32;
+}
