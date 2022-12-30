5d501a47ade7a762c3b091686fc3e2168aa36b6a
David Lebee <david.lebee@gluwa.com>
2022-12-30 17:03:49 -0500

    using struct.

--- enum2/src/main.rs
+++ enum2/src/main.rs
@@ -3,7 +3,11 @@ enum Transaction
     Transfer(String, u128),
     Approve(String, u128),
     Mint(u128),
-    Burn(u128)
+    Burn(u128),
+    IncreaseAllowance{
+        destination: String,
+        amount: u128
+    }
 }
 
 fn print_transaction(transaction: &Transaction)
@@ -20,6 +24,12 @@ fn print_transaction(transaction: &Transaction)
         },
         Transaction::Burn(amount) => {
             println!("burn: {}", amount);
+        },
+        Transaction::IncreaseAllowance { 
+            destination,
+            amount
+        } => {
+            println!("Increase allowance: {} {}", destination, amount);
         }
     }
 }
@@ -29,9 +39,14 @@ fn main() {
     let approve = Transaction::Approve(String::from("0x01"), 120_u128);
     let mint = Transaction::Mint(100_u128);
     let burn = Transaction::Burn(120_u128);
+    let increase_allowance = Transaction::IncreaseAllowance {
+        destination: String::from("0x02"),
+        amount: 200_u128
+    };
 
     print_transaction(&transfer);
     print_transaction(&approve);
     print_transaction(&mint);
     print_transaction(&burn);
+    print_transaction(&increase_allowance);
 }
