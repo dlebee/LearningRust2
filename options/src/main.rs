a9464e8753224e6cda7d5077e32ab76cc2691438
David Lebee <david.lebee@gluwa.com>
2022-12-30 14:30:39 -0500

    options

---
+++ options/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let phrase = String::from("Duck Airlines");
+    let letter = phrase.chars().nth(5);
+
+    let value = match letter {
+        Some(character) => character.to_string(),
+        None => String::from("No value")
+    };
+
+    println!("{}", value);
+}
