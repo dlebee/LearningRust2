37885fe752ca12f8f6f3d560d5993fd72f58aa9e
David Lebee <david@lebee.info>
2024-02-26 17:18:29 -0500

    using a shared db using Arc and mutex

--- tokio-my-redis-server/src/main.rs
+++ tokio-my-redis-server/src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
 
@@ -6,23 +9,27 @@ async fn main() {
     // Bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening!");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // The second item contains the IP and port of the new connection.
         let (socket, _) = listener.accept().await.unwrap();
         // A new task is spawned for each inbound socket. The socket is
         // moved to the new task and processed there.
+        let cloned_db = db.clone();
+
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, cloned_db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Arc<Mutex<HashMap<String, Vec<u8>>>>) {
 
     use mini_redis::Command::{self, Get, Set};
     use std::collections::HashMap;
-
-    let mut db = HashMap::new();
 
     // The `Connection` lets us read/write redis **frames** instead of
     // byte streams. The `Connection` type is defined by mini-redis.
@@ -31,10 +38,12 @@ async fn process(socket: TcpStream) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
+                let mut db = db.lock().unwrap();
                 db.insert(cmd.key().to_string(), cmd.value().to_vec());
                 Frame::Simple("OK".to_string())
             },
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     Frame::Bulk(value.clone().into())
                 } else {
